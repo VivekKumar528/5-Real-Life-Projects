b31b1a9370681fe32d09623bea07b322abff861a
Vivek Kumar <vicky2820x@gmail.com>
2025-03-12 00:39:42 +0530

    Update : Single Button Functionality

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var istatus = document.querySelector("h5");
 
-var addFriend = document.querySelector("#add");
-var removeFriend = document.querySelector("#remove");
+var btn = document.querySelector("button");
 
-addFriend.addEventListener("click", function () {
-  istatus.innerHTML = "Friends";
-  istatus.style.color = "green";
-});
-
-removeFriend.addEventListener("click", function () {
-  istatus.innerHTML = "Stranger";
-  istatus.style.color = "red";
+var check = 0;
+btn.addEventListener("click", function () {
+  if (check == 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+    btn.innerHTML = "Remove Friend";
+    check = 1;
+  } else {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "red";
+    btn.innerHTML = "Add Friend";
+    check = 0;
+  }
 });
